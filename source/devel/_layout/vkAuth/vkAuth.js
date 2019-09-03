20c356c96d5d21d22022a8603fb510903220f126
Ragnarok <DeathMetallDrummer@gmail.com>
2019-09-04 01:04:36 +0600

    123

--- source/devel/_layout/vkAuth/vkAuth.js
+++ source/devel/_layout/vkAuth/vkAuth.js
@@ -3,17 +3,21 @@ $(function () {
 });
 
 function vkSignIn() {
+  window.vkUsers = {};
   VK.Auth.login(function (response) {
     // console.log(response);
 
 
-    // VK.Api.call('users.get', {user_ids: response.session.mid,fields: "photo_50", v:"5.73"}, function(r) {
-    //   if(r.response) {
-    //     alert('Привет, ' + r.response[0].photo_50);
-    //   }
-    // });
+    VK.Api.call('users.get', {user_ids: response.session.mid,fields: "photo_max_orig,last_seen", v:"5.73"}, function(r) {
+      if(r.response) {
+        vkUsers.avatar = r.response[0].photo_max_orig;
+        vkUsers.name = r.response[0].first_name + '' + r.response[0].last_name;
+        vkUsers.desc = r.response[0].last_seen.time;
+        vkViewInfo(response.status)
+      }
+    });
 
-    vkViewInfo(response.status)
+
   });
 }
 
@@ -27,8 +31,8 @@ function vkSignOut() {
 
 function vkViewInfo(status) {
   if (status !== 'connected') {
-
+    clearUserInfo();
   } else {
-
+    userInfo(vkUsers.avatar,vkUsers.name,vkUsers.desc);
   }
 }
