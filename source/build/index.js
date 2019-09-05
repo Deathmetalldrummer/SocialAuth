540f11041466bded4a78fda261cc904142f2970b
Kirill Terekhin <kirill.terekhin@zensoft.io>
2019-09-05 14:15:10 +0600

    123

--- source/build/index.js
+++ source/build/index.js
@@ -87,20 +87,20 @@ function vkViewInfo(status) {
   }
 }
 
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '2722005527823279',
-    cookie     : true,
-    xfbml      : true,
-    version    : 'v4.0'
-  });
-};
-
-function vkSingIn() {
-  window.FB.getLoginStatus(function(response) {
-    console.log(true);
-    statusChangeCallback(response);
-  });
-}
+// window.fbAsyncInit = function() {
+//   FB.init({
+//     appId      : '2722005527823279',
+//     cookie     : true,
+//     xfbml      : true,
+//     version    : 'v4.0'
+//   });
+// };
+//
+// function vkSingIn() {
+//   window.FB.getLoginStatus(function(response) {
+//     console.log(true);
+//     statusChangeCallback(response);
+//   });
+// }
 
 
