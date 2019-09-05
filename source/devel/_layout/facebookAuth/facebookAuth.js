c89072a69d2a0e40542c1008800f2d830ad34fbc
Kirill Terekhin <kirill.terekhin@zensoft.io>
2019-09-05 13:40:44 +0600

    123

---
+++ source/devel/_layout/facebookAuth/facebookAuth.js
@@ -0,0 +1,15 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '2722005527823279',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v4.0'
+  });
+};
+
+function vkSingIn() {
+  window.FB.getLoginStatus(function(response) {
+    console.log(true);
+    statusChangeCallback(response);
+  });
+}
