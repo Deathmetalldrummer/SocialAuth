744b821509df8616660dafecb0eb233909c3e62c
Kirill Terekhin <kirill.terekhin@zensoft.io>
2019-09-06 14:38:17 +0600

    123

--- source/devel/_layout/facebookAuth/facebookAuth.js
+++ source/devel/_layout/facebookAuth/facebookAuth.js
@@ -1,21 +1,36 @@
-window.fbAsyncInit = function() {
+$(function () {
   FB.init({
     appId      : '2722005527823279',
     cookie     : true,
     xfbml      : true,
     version    : 'v4.0'
   });
-};
+});
 
 function fbSignIn() {
   FB.login(function(response) {
     if (response.authResponse) {
       console.log('Welcome!  Fetching your information.... ',response);
       FB.api('/me', function(response) {
-        console.log('Good to see you, ' + response.name + '.');
+        console.log('Good to see you, ' , response , '.');
+var name = response.name;
+var desc = response.id;
+        FB.api(
+          '/'+response.id+'/picture',
+          'GET',
+          {"redirect":"false"},
+          function(response) {
+            var avatar = response.data.url;
+            userInfo(avatar,name,desc);
+
+          }
+        );
+
+
       });
     } else {
       console.log('User cancelled login or did not fully authorize.');
     }
   });
+  console.log(true);
 }
