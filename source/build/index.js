f4364c9886381141a947853e5d103c43e0c43687
Kirill Terekhin <kirill.terekhin@zensoft.io>
2019-09-06 13:49:31 +0600

    123

--- source/build/index.js
+++ source/build/index.js
@@ -87,21 +87,27 @@ function vkViewInfo(status) {
   }
 }
 
-// window.fbAsyncInit = function() {
-//   FB.init({
-//     appId      : '2722005527823279',
-//     cookie     : true,
-//     xfbml      : true,
-//     version    : 'v4.0'
-//   });
-// };
-//
-// function fbSignIn() {
-//   window.FB.getLoginStatus(function(response) {
-//     console.log(true);
-//     statusChangeCallback(response);
-//   });
-// }
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '2722005527823279',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v4.0'
+  });
+};
+
+function fbSignIn() {
+  FB.login(function(response) {
+    if (response.authResponse) {
+      console.log('Welcome!  Fetching your information.... ',response);
+      FB.api('/me', function(response) {
+        console.log('Good to see you, ' + response.name + '.');
+      });
+    } else {
+      console.log('User cancelled login or did not fully authorize.');
+    }
+  });
+}
 
 function ghSingIn() {
   OAuth.initialize('ATBfZdbaUFfsJY_TDk82H87HbLE');
--- source/devel/_layout/facebookAuth/facebookAuth.js
+++ source/devel/_layout/facebookAuth/facebookAuth.js
@@ -1,15 +1,21 @@
-// window.fbAsyncInit = function() {
-//   FB.init({
-//     appId      : '2722005527823279',
-//     cookie     : true,
-//     xfbml      : true,
-//     version    : 'v4.0'
-//   });
-// };
-//
-// function fbSignIn() {
-//   window.FB.getLoginStatus(function(response) {
-//     console.log(true);
-//     statusChangeCallback(response);
-//   });
-// }
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '2722005527823279',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v4.0'
+  });
+};
+
+function fbSignIn() {
+  FB.login(function(response) {
+    if (response.authResponse) {
+      console.log('Welcome!  Fetching your information.... ',response);
+      FB.api('/me', function(response) {
+        console.log('Good to see you, ' + response.name + '.');
+      });
+    } else {
+      console.log('User cancelled login or did not fully authorize.');
+    }
+  });
+}
